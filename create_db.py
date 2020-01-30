f76881434686ba182db4dd2af658c156dcd22bf3
Daniel <danielmichalik98@wp.pl>
2020-01-30 21:52:18 +0100

    change create_db file

--- create_db.py
+++ create_db.py
@@ -4,18 +4,31 @@ from sqlalchemy import Column, Float, String, Integer, DateTime, create_engine
 Base = declarative_base()
 
 
+def create_database():
+    engine = create_engine('mysql://root:*******@localhost')
+    engine.execute("CREATE DATABASE exp_db")
+
+
 class Expenses(Base):
     __tablename__ = "Expenses"
     id = Column(Integer, primary_key=True, name="id")
     price = Column(Float, name="price")
     date = Column(DateTime, name="date")
-    name = Column(String, name="name")
-    category = Column(String, name="category")
+    name = Column(String(50), name="name")
+    category = Column(String(50), name="category")
 
     def __repr__(self):
         return f"<Expenses(price={self.price}, date={self.date}, name={self.name}, category={self.category})>"
 
 
+def get_engine():
+    engine = create_engine(
+        "mysql+pymysql://root:*******@localhost/exp_db"
+    )
+    return engine
+
+
 def create_table():
-    engine = create_engine("sqlite:///:memory:", echo=True)
+    engine = create_engine(
+        "mysql+pymysql://root:*******@localhost/exp_db")
     Base.metadata.create_all(engine)
